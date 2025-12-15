a94510846a70532175c5d8ca996568115771b2c1
shingyucheoll <fresky16@gmail.com>
2025-12-16 00:18:53 +0900

    Fork/Join 프레임워크2 작업 훔치기
    임계값 설정을 더 낮게 설정하여 ( 4 -> 2 ) 각 worker 내부에서 다시 fork() -> 병렬 처리
    THRESHOLD 가 4에서 2로 변경됨에 따라 동작하는 worker가 2개에서 4개로 변경되며
    4초 걸리던 작업이 병렬로 실행되어 2초로 변경됩니다.
    
    THRESHOLD = 2 일 때 프로세스 과정을 정리하면
    처음 Main Thread 에서 SumTask task = new SumTask(data) 를 선언함에 따라
    작업이 8개인 SumTask가 생성이 되고
    Thread 가 10개가 설정된 ForkJoinPool로 invoke() -> 호출, 즉 실행을 하게 됩니다.
    
    invoke(task) 메서드를 통해 SumTask가 동작하게 되는데 이 과정은 아래와 같습니다.
    
    worker1 로 작업을 시작하여 if문의 조건식을 만나게 되는데 작업량이 임계값보다 많기 때문에
    else문이 실행되어
    
    worker1에서 leftTask 와 rightTask 가 새로 생성되게 됩니다.
    
    이 때
    leftTask는 새로운 스레드에서 작업을 시작하도록 지시하고 - fork();
    rightTask는 현재 스레드에서 재귀호출을 통해 다시 compute()를 호출하는 구조를 갖습니다.
    
    이 때 worker1의 상태는
    leftTask.fork()를 통해 ForkJoinPool 내부 가용 Thread를 사용하여 병렬 처리를 시작하고,
    재귀적으로 compute()를 호출한 상태가 됩니다.
    
    fork()를 통해 생성된 worker2와 compute()를 재귀호출하여 다시 작업을 시작한 worker1은
    아래와 같이 작업을 갖게 됩니다.
    worker1 = [5,6,7,8]
    worker2 = [1,2,3,4]
    
    해당 상태에서 compute()가 실행되면 처음 실행했을 때와 동일하게
    if (list.size() <= THRESHOLD) 조건을 만족하지 못하게 되고 else문이 실행되어
    
    worker1은 다시 아래와 같이 Task 가 2개로 나누어 지며
    leftTask = [5,6]
    rightTask = [7,8]
    leftTask 는 다시 fork() -> worker3 생성
    
    worker2 에서도 동일하게 Task 가 2개로 나누어지며
    leftTask = [1,2]
    rightTask = [3,4]
    leftTask 는 worker2 에서 fork()를 호출하여 worker4를 생성하게 됩니다.
    
    ( 이 때, worker1은 이미 한 번 compute() 를 재귀호출한 상태임을 기억해주셔야 합니다. )
    
    다음으로 worker1, worker2, worker3, worker4 각각 compute() 메서드를 호출하는 형태가 되며
    
    이제 4개의 worker는 작업량이 임계값보다 같거나 작기 때문에
    
    더이상 분할하지 않고 작업을 시작할 수 있게 됩니다. list.size() <= THRESHOLD = true
    
    이 때 각 worker의 상태를 정리하면
    
    worker1은
    leftTask.fork() 호출 -> worker2 생성 및 작업 지시
    rightTask.compute() 호출 -> 재귀호출을 통해 다시 rightTask 의 작업을 가진 상태로 실행
    
    - if 조건문 거짓으로 else문 실행
    
    leftTask.fork() 호출 -> worker3 생성 및 작업 지시
    rightTask.compute() 호출 -> 재귀호출을 통해 다시 rightTask 의 작업을 가진 상태로 실행
    
    자신의 작업을 완료하여 rightResult 값을 저장한 상태로
    worker3의 작업을 join() 동기식으로 기다림
    
    worker3의 작업과 현재 작업의 결과값을 반환,
    
    이 때 worker2에게 작업을 지시 후 재귀호출한 compute() 의 작업이 완료
    rightResult에 worker1 재귀호출을 통해 생성된 결과값과, worker3의 결과값을 합쳐 저장
    
    그 이후 해당 시점에 작업을 지시했언 worker2 의 작업을 join()을 통해 기다림
    
    worker2의 결과값을 받은 후 모든 재귀 호출이 끝나 Main Thread의 invoke() 결과값으로 응답
    
    ---
    
    worker2 내부 과정은 worker1 에서 worker3을 생성하며 관리했던 것과 동일함
    
    따라서
    
    현재 로직에 대한 로그를 출력하게 되면
    재귀호출로 인해 worker1은 joinSum을 2번 호출하는걸 볼 수 있습니다.

--- src/parallel/forkjoin/SumTask.java
+++ src/parallel/forkjoin/SumTask.java
@@ -9,49 +9,50 @@ import parallel.HeavyJob;
 
 public class SumTask extends RecursiveTask<Integer> {
 
-    private static final int THRESHOLD = 4; // 임계값
+	// private static final int THRESHOLD = 4; // 임계값
+	private static final int THRESHOLD = 2; // 임계값 변경
 
-    private final List<Integer> list;
+	private final List<Integer> list;
 
-    public SumTask(List<Integer> list) {
-        this.list = list;
-    }
+	public SumTask(List<Integer> list) {
+		this.list = list;
+	}
 
-    @Override
-    protected Integer compute() {
+	@Override
+	protected Integer compute() {
 
-        // 작업 범위가 임계값보다 작을 경우 직접 계산
-        if(list.size() <= THRESHOLD) {
+		// 작업 범위가 임계값보다 작을 경우 직접 계산
+		if (list.size() <= THRESHOLD) {
 
-            log("[처리 시작] " + list);
+			log("[처리 시작] " + list);
 
-            int sum = list.stream()
-                .mapToInt(HeavyJob::heavyTask)
-                .sum();
+			int sum = list.stream()
+				.mapToInt(HeavyJob::heavyTask)
+				.sum();
 
-            log("[처리 완료] " + list + " -> sum: " + sum);
-            return sum;
-        } else {
-            // 작업 범위가 크면 반으로 나누어 병렬 처리
-            int mid = list.size() / 2;
-            List<Integer> leftList = list.subList(0, mid); // 0 ~ mid 사이즈전까지
-            List<Integer> rightList = list.subList(mid, list.size());  // mid 사이즈부터 끝까지7
-            log("[분할] " + list + " -> leftList: " + leftList + ", rightList: " + rightList);
+			log("[처리 완료] " + list + " -> sum: " + sum);
+			return sum;
+		} else {
+			// 작업 범위가 크면 반으로 나누어 병렬 처리
+			int mid = list.size() / 2;
+			List<Integer> leftList = list.subList(0, mid); // 0 ~ mid 사이즈전까지
+			List<Integer> rightList = list.subList(mid, list.size());  // mid 사이즈부터 끝까지7
+			log("[분할] " + list + " -> leftList: " + leftList + ", rightList: " + rightList);
 
-            SumTask leftTask = new SumTask(leftList);
-            SumTask rightTask = new SumTask(rightList);
+			SumTask leftTask = new SumTask(leftList);
+			SumTask rightTask = new SumTask(rightList);
 
-            // 왼쪽 작업은 다른 스레드에서 처리
-            leftTask.fork();
-            // 오른쪽 작업은 현재 스레드에서 직접 처리 (compute() 로 실행하여 else 문에 도달 후 다시 compute()를 현재 스레드에서 호출한다 -> (재귀호출))
-            // 작업 갯수가 8개인 상태에서 / 2 로 생성된 Task 로 compute()를 호출하게 되면 if문 조건이 성립하여 직접 작업이 실행된다.
-            Integer rightResult = rightTask.compute();
+			// 왼쪽 작업은 다른 스레드에서 처리
+			leftTask.fork();
+			// 오른쪽 작업은 현재 스레드에서 직접 처리 (compute() 로 실행하여 else 문에 도달 후 다시 compute()를 현재 스레드에서 호출한다 -> (재귀호출))
+			// 작업 갯수가 8개인 상태에서 / 2 로 생성된 Task 로 compute()를 호출하게 되면 if문 조건이 성립하여 직접 작업이 실행된다.
+			Integer rightResult = rightTask.compute();
 
-            // 왼쪽 작업 결과를 기다립니다.
-            Integer leftResult = leftTask.join();
-            int joinSum = leftResult + rightResult;
-            log("LEFT[ + " + leftResult + "] + RIGHT [ " + rightResult + "] -> sum = " + joinSum);
-            return joinSum;
-        }
-    }
+			// 왼쪽 작업 결과를 기다립니다.
+			Integer leftResult = leftTask.join();
+			int joinSum = leftResult + rightResult;
+			log("LEFT[ + " + leftResult + "] + RIGHT [ " + rightResult + "] -> joinSum = " + joinSum);
+			return joinSum;
+		}
+	}
 }
